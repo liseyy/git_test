8c5225b2cd47611c7a86ef715c0ce4680188da41
yonghong-chen <islandside188@163.com>
2014-08-18 22:43:33 +0800

    Modify main program.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ extern int test();
 
 int main()
 {
+	printf ("This is a git test program!");
 	test();
 	return 0;
 }
--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int test(void); // just print
+
 int test()
 {
     printf("Hello, This is a test!\n");
